b5f1b30463f2ae3cc13503c3e19755631d9b199e
ThemiyaRajapaksha <155937499+ThemiyaRajapaksha@users.noreply.github.com>
2024-01-08 14:38:22 +0530

    Add files via upload

---
+++ Project.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Menu item name is required'],
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'Menu item price is required'],
+    min: [0, 'Price cannot be negative']
+  },
+  category: {
+    type: String,
+    required: [true, 'Menu item category is required'],
+    enum: ['seafood', 'rice_curries', 'street_foods', 'vegetarian', 'sweets'],
+  },
+  image: {
+    type: String,
+    default: null 
+  }
+}, {
+  timestamps: true 
+});
+
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema);
+
+module.exports = MenuItem;
