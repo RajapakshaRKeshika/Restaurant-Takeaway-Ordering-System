35bfbcfdb6897c00878d1c6f0b6c355727f9d8cd
Sudeepa996 <155929689+Sudeepa996@users.noreply.github.com>
2024-01-08 20:15:58 +0530

    Add files via upload

---
+++ promotion.js
@@ -0,0 +1,10 @@
+
+const mongoose = require('mongoose');
+
+const promotionSchema = new mongoose.Schema({
+  name: String,
+  imageUrl: String,
+
+});
+
+module.exports = mongoose.model('Promotion', promotionSchema);
